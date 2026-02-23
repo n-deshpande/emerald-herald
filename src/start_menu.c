51d17758af6f459df351c0cf99c5eecdd751d69c
Nish <nxdeshpande@gmail.com>
2026-02-23 20:56:35 +1100

    Fix merge conflicts curse menu

--- src/start_menu.c
+++ src/start_menu.c
@@ -339,7 +339,7 @@ static void BuildNormalStartMenu(void)
     if (FlagGet(FLAG_SYS_POKEDEX_GET) == TRUE)
         AddStartMenuAction(MENU_ACTION_POKEDEX);
 
-    if (DN_FLAG_DEXNAV_GET != 0 && FlagGet(DN_FLAG_DEXNAV_GET))
+    if (!showCurses && DN_FLAG_DEXNAV_GET != 0 && FlagGet(DN_FLAG_DEXNAV_GET))
         AddStartMenuAction(MENU_ACTION_DEXNAV);
 
     if (FlagGet(FLAG_SYS_POKEMON_GET) == TRUE)
