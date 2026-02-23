afd363ffb9044ab3d1a6a41218f954a923db7559
Nishant Deshpande <nishant.deshpande@7eleven.com.au>
2026-02-23 16:24:47 +1100

    Add curse journal start menu

---
+++ include/curse_menu.h
@@ -0,0 +1,6 @@
+#ifndef GUARD_CURSE_MENU_H
+#define GUARD_CURSE_MENU_H
+
+void ShowCurseMenu(void (*callback)(void));
+
+#endif // GUARD_CURSE_MENU_H
---
+++ test/curse.c
@@ -0,0 +1,39 @@
+#include "global.h"
+#include "curse.h"
+#include "test/test.h"
+
+TEST("Curse_ClearActive clears boon and bane slots")
+{
+    Curse_SetActiveBoon(0, CURSE_BOON_BULWARK);
+    Curse_SetActiveBane(0, CURSE_BOON_BULWARK);
+    Curse_ClearActive();
+
+    EXPECT_EQ(Curse_GetActiveBoon(0), CURSE_NONE);
+    EXPECT_EQ(Curse_GetActiveBane(0), CURSE_NONE);
+}
+
+TEST("Curse_SetActiveBane sets the target slot")
+{
+    Curse_ClearActive();
+    Curse_SetActiveBane(1, CURSE_BOON_BULWARK);
+
+    EXPECT_EQ(Curse_GetActiveBane(1), CURSE_BOON_BULWARK);
+}
+
+TEST("Curse active getters return CURSE_NONE for invalid slots")
+{
+    Curse_ClearActive();
+
+    EXPECT_EQ(Curse_GetActiveBoon(CURSE_ACTIVE_BOON_SLOTS), CURSE_NONE);
+    EXPECT_EQ(Curse_GetActiveBane(CURSE_ACTIVE_BANE_SLOTS), CURSE_NONE);
+}
+
+TEST("Curse active getters return set values for valid slots")
+{
+    Curse_ClearActive();
+    Curse_SetActiveBoon(2, CURSE_BOON_BULWARK);
+    Curse_SetActiveBane(2, CURSE_BOON_BULWARK);
+
+    EXPECT_EQ(Curse_GetActiveBoon(2), CURSE_BOON_BULWARK);
+    EXPECT_EQ(Curse_GetActiveBane(2), CURSE_BOON_BULWARK);
+}
